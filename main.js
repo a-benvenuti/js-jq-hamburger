c62298e68cb947767dc44e8f9148168239b7405b
Andrea <b.a.benvenuti3@gmail.com>
2021-03-11 15:39:59 +0100

    Aggiunte prove

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+var hamburgerMenu = $('.hamburger-menu');
+
 $('.header-right > a').click(
   function(){
-    $('.hamburger-menu').fadeIn(300);
+    hamburgerMenu.fadeIn(300);
   }
 );
 
 $('.close').click(
   function(){
-    $('.hamburger-menu').fadeOut(300);
+    hamburgerMenu.fadeOut(300);
+  }
+);
+
+// EXTRA / PROVE
+var btn = $(".header-right ul li a.cta");
+
+btn.mouseenter(
+  function(){
+    btn.text("inizia ora!")
+  }
+);
+btn.mouseleave(
+  function(){
+    btn.text("Iscriviti")
   }
 );
